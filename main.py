c3edf890e07914aecb4da9a98108f656ba85c877
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-05 23:51:06 +0530

    main

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ while game_is_on:
     # detecting the collison
     if ball.ycor() > 290 or ball.ycor() < -280:
         # bounce
-        ball.bounce()
+        ball.bounce_y()
+
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() > -320):
+        ball.bounce_x()
 
 screen.exitonclick()
