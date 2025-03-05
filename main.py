d4cf39a6a0c7b2adc9af6c4d054a64bd1145fd74
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-05 23:23:20 +0530

    commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from turtle import Screen, Turtle
 from Paddle import Paddle
-
+from ball import Ball
 
 screen = Screen()
 screen.setup(800,600)
@@ -9,6 +9,7 @@ screen.title("PING-PING")
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up,"Up")
@@ -20,6 +21,6 @@ screen.onkey(l_paddle.go_down,"s")
 game_is_on = True
 while game_is_on:
     screen.update()
-
+    ball.move()
 
 screen.exitonclick()
